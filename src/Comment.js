d0fe3bf05d10f5f2cd0ef4e4d67961f8393aad1e
Felipe Vieira <flpchapola@hotmail.com>
2017-09-23 17:48:34 -0300

    iterando as mensagems e separando os componentes comentarios e comentario

--- src/Comment.js
+++ src/Comment.js
@@ -3,16 +3,16 @@ import React, {Component} from 'react'
 class Comment extends Component {
 		render() {
 				return (
-								<li>
-										<div className="commenterImage">
-												<img src="http://lorempixel.com/50/50/people/9"/>
-										</div>
-										<div className="commentText">
-												<p className="">Titulo</p>
-												<span className="date sub-text">em 23 de Setembro de 2017</span>
+						<li>
+								<div className="commenterImage">
+										<img src="http://lorempixel.com/50/50/people/9"/>
+								</div>
+								<div className="commentText">
+										<p className="">Titulo</p>
+										<span className="date sub-text">{this.props.comment.comment}</span>
 
-										</div>
-								</li>
+								</div>
+						</li>
 				)
 		}
 }
--- src/Comments.js
+++ src/Comments.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react'
 
 import Comment from './Comment'
-
 class Comments extends Component {
+
+		renderComment(key, comment) {
+				return (
+						<Comment key={key} comment={comment} />
+				)
+		}
 		render() {
 				return (
-					<ul className="commentList">
-					{JSON.stringify(this.props.comments)}
-						<Comment/>
-					</ul>
+						<ul className="commentList">
+								{Object.keys(this.props.comments)
+										.map(key => this.renderComment(key, this.props.comments[key]))}
+						</ul>
 				)
 		}
 }
